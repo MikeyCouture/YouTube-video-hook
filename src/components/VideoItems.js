cf4f59b39b3e496f10f0a3f4240be52665f78de8
MikeyCouture <michelcouture42@gmail.com>
2021-04-11 13:31:38 -0400

    initial commit

---
+++ src/components/VideoItems.js
@@ -0,0 +1,26 @@
+import "./VideoItem.css";
+import React from "react";
+
+const VideoItems = ({ video, onVideoSelect }) => {
+  //this.props.video <--avail here
+
+  return (
+    <div
+      onClick={() => {
+        onVideoSelect(video);
+      }}
+      className="video-item item"
+    >
+      <img
+        alt={video.snippet.title}
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+      />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoItems;
