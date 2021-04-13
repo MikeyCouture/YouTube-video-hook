a32768598ebc00924c03462219b8fe8609d5ad53
MikeyCouture <michelcouture42@gmail.com>
2021-04-13 19:15:50 -0400

    Starting to add footer and header

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
 import useVideos from "../hooks/useVideos";
+import Footer from "./Footer";
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
@@ -13,21 +14,24 @@ const App = () => {
   }, [videos]);
 
   return (
-    <div className="ui container">
-      <SearchBar onFormSubmit={search} />
-      <div className="ui grid">
-        <div className="ui row">
-          <div className="eleven wide column">
-            <VideoDetail video={selectedVideo} />
-          </div>
-          <div className="five wide column">
-            <VideoList
-              onVideoSelect={video => setSelectedVideo(video)}
-              videos={videos}
-            />
+    <div className="main-container">
+      <div className="ui container">
+        <SearchBar onFormSubmit={search} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={video => setSelectedVideo(video)}
+                videos={videos}
+              />
+            </div>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div>
+        <p>
+          <a target="_blank" href="https://www.udemy.com/user/sgslo/">
+            Proudly developed alongside Stephen Grider's Udemy Course
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
